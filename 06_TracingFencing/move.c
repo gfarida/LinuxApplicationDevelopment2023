cbf974bd9076ce18b78d66a62b6d8f63fe784b63
gfarida <faridafarida378@gmail.com>
2023-10-30 23:54:38 +0300

    added task 6

--- 06_TracingFencing/move.c
+++ 06_TracingFencing/move.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
 
     if (access(argv[2], F_OK) != -1) {
         if (truncate(argv[2], 0) == -1) {
-            fprintf(stderr, "Can't clean file %s with error %s\n", 
+            fprintf(stderr, "Can't clean file %s with error: %s\n", 
                     argv[2], strerror(errno));
             exit(errno);
         }
@@ -23,14 +23,14 @@ int main(int argc, char *argv[]) {
 
     in = fopen(argv[1], "r");
     if (in == NULL) {
-        fprintf(stderr, "Can't open file %s with error %s\n", 
+        fprintf(stderr, "Can't open file %s with error: %s\n", 
                 argv[1], strerror(errno));
         exit(errno);
     }
 
     out = fopen(argv[2], "w");
     if (out == NULL) {
-        fprintf(stderr, "Can't open file %s with error %s\n", 
+        fprintf(stderr, "Can't open file %s with error: %s\n", 
                 argv[2], strerror(errno));
         fclose(in);
         exit(errno);
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]) {
 
     while ((c = fgetc(in)) != EOF) {
         if (fputc(c, out) == EOF) {
-            fprintf(stderr, "Can't write symbol to %s with error %s\n", 
+            fprintf(stderr, "Can't write symbol to %s with error: %s\n", 
                     argv[2], strerror(errno));
             fclose(in);
             fclose(out);
@@ -52,7 +52,7 @@ int main(int argc, char *argv[]) {
 
 
     if (remove(argv[1]) != 0) {
-        fprintf(stderr, "Can't remove file %s with error %s\n", 
+        fprintf(stderr, "Can't remove file %s with error: %s\n", 
                 argv[1], strerror(errno));
         remove(argv[2]);
         exit(errno);
--- 06_TracingFencing/noremove.c
+++ 06_TracingFencing/noremove.c
@@ -1,12 +1,14 @@
+#define _GNU_SOURCE
 #include <dlfcn.h>
 #include <string.h>
+
 
 typedef int (*remove_type)(void);
 
 int remove(char *file) {
     if (strstr(file, "PROTECT") == 0) {
-        return ((remove_type)(dlsym(RTLD_NEXT, "remove")))(file);
+        return ((remove_type)(dlsym(RTLD_NEXT, "remove")))();
     } else {
-        return -1;
+        return 0;
     }
 }
